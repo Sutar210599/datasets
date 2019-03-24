df50170a77aa57495b00acbd7cd188fa3e1e0692
Sutar <gouravsutar@gmail.com>
2019-03-24 15:02:09 +0530

    Import tensorflow functions

--- tensorflow_datasets/text/snli.py
+++ tensorflow_datasets/text/snli.py
@@ -15,5 +15,11 @@
 
 """ The Stanford Natural Language Inference (SNLI) Corpus"""
 from __future__ import absolute_import
-from __furtur__ import division
+from __future__ import division
 from __future__ import print_function
+
+import os
+
+import tensorflow as tf
+from tensorflow_datasets.core import api_utils
+import tensorflow_datasets.public_api as tfds
